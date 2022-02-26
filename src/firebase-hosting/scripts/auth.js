f69613ae3b6e384cb33c3eb736e05ed1e8f0a00a
Tavie Kittredge <taviekit@gmail.com>
2022-02-26 17:11:14 -0500

    colored navbar

---
+++ src/firebase-hosting/scripts/auth.js
@@ -0,0 +1,106 @@
+var firebaseConfig = {
+      
+    apiKey: "AIzaSyAMHutTkg_Pp54iMrYDX3NqDLhRhJo2Lyw",
+
+    authDomain: "college-foodie.firebaseapp.com",
+
+    projectId: "college-foodie",
+
+    storageBucket: "college-foodie.appspot.com",
+
+    messagingSenderId: "1072876126182",
+
+    appId: "1:1072876126182:web:87a17b1ad3a26b8d89e027",
+
+    measurementId: "G-8LWH2KZ4DD"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+let unsubscribe = () => {};
+auth.onAuthStateChanged(user => {
+    if (user) {    
+        unsubscribe =  db.collection('guides').onSnapshot(snapshot => {
+            setupGuides(snapshot.docs);
+            snapshot.docs.forEach(doc => {
+            const guide = doc.data();
+            db.collections('users').add({
+                title: guide.title,
+                content: guide.content,
+            })
+            })     
+            html += li;
+            
+        })
+        setupUI(user);
+    } else {
+        setupUI();
+        setupGuides([]);
+        unsubscribe();
+    }
+});
+//create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value,
+        user: "haha",
+    }).then(() => {
+        //close the modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset;
+    }). catch (err => {
+        console.log(err.message);
+    })
+})
+//sign up
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //get user info
+
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value
+        })
+       
+    }).then(() => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset;
+    });
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    //get user email
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset;
+    });
+});
